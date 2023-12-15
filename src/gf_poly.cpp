f6dcdb721c348f00dfadf55b38986fae969d2ccd
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2023-12-15 21:55:45 +0100

    galois field polynomial divition

--- src/gf_poly.cpp
+++ src/gf_poly.cpp
@@ -2,13 +2,14 @@
 
 #include "gf.h"
 
-void gf_poly_scale(std::vector<uint8_t>& poly, uint8_t scalor) {
+void gf_poly_scale(std::vector<uint8_t> &poly, uint8_t scalor) {
     for (int i = 0; i < poly.size(); i++) {
         poly[i] = gf_mul(poly[i], scalor);
     }
 }
 
-std::vector<uint8_t> gf_poly_add(std::vector<uint8_t>& x, std::vector<uint8_t>& y) {
+std::vector<uint8_t> gf_poly_add(std::vector<uint8_t> &x,
+                                 std::vector<uint8_t> &y) {
     std::vector<uint8_t> res;
     res.resize(std::max(x.size(), y.size()));
 
@@ -21,7 +22,8 @@ std::vector<uint8_t> gf_poly_add(std::vector<uint8_t>& x, std::vector<uint8_t>& 
     return res;
 }
 
-std::vector<uint8_t> gf_poly_mult(std::vector<uint8_t>& x, std::vector<uint8_t>& y) {
+std::vector<uint8_t> gf_poly_mul(std::vector<uint8_t> &x,
+                                 std::vector<uint8_t> &y) {
     std::vector<uint8_t> res;
     res.resize(x.size() + y.size() - 1);
 
@@ -33,7 +35,32 @@ std::vector<uint8_t> gf_poly_mult(std::vector<uint8_t>& x, std::vector<uint8_t>&
     return res;
 }
 
-uint8_t gf_poly_eval(std::vector<uint8_t>& poly, uint8_t x) {
+std::tuple<std::vector<uint8_t>, std::vector<uint8_t>>
+gf_poly_div(std::vector<uint8_t> &dividend, std::vector<uint8_t> &divisor) {
+    std::vector res = dividend; // Copy dividend
+    for (int i = 0; i < dividend.size() - divisor.size() - 1; i++) {
+        uint8_t coef = res[i];
+        if (coef != 0) {
+            for (int j = 1; j < divisor.size(); j++) {
+                if (divisor[j] != 0)
+                    res[i + j] ^= gf_mul(divisor[j], coef);
+            }
+        }
+    }
+
+    int seperator = -(divisor.size() - 1);
+    std::vector<uint8_t> quotient;
+    std::vector<uint8_t> remainder;
+    quotient.resize(seperator);
+    remainder.resize(res.size() - seperator);
+
+    quotient.assign(res.begin(), res.begin() + seperator);
+    remainder.assign(res.begin() + seperator + 1, res.end());
+
+    return { quotient, remainder };
+}
+
+uint8_t gf_poly_eval(std::vector<uint8_t> &poly, uint8_t x) {
     uint8_t y = poly[0];
     for (int i = 1; i < poly.size(); i++) {
         y = gf_mul(y, x) ^ poly[i];
